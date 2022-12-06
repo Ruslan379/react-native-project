42208606844fd0fe3181c73056c1288c4f173443
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2022-12-07 01:55:05 +0200

    Work HW-2.4 *19

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { StatusBar } from 'expo-status-bar';
+
 import {
   StyleSheet,
   Text,
   View,
   ImageBackground,
+  TextInput,
 } from 'react-native';
 
 
@@ -14,25 +15,12 @@ export default function App() {
     <View style={styles.container}>
       <ImageBackground
         style={styles.image}
-        source={require("./assets/images/stars-on-night-2.jpg")}>
+        source={require("./assets/images/stars-on-night.jpg")}
+      >
 
-        {/* <Text style={{ color: "green", fontSize: 20 }}>Open up App.js to start working on your app!</Text>
-        <Text style={{ color: "red", fontSize: 25 }}>Open up App.js to start working on your app twice!</Text>
-        <Text style={{ color: "blue", fontSize: 30 }}>Open up App.js to start working on your app thrice!</Text> */}
-
-        <View style={styles.innerBoxFirst}>
-          <Text style={styles.textFirst}>Open App.js to start working on your app for the FIRST time!</Text>
-        </View>
-
-        <View style={styles.innerBoxSecond}>
-          <Text style={styles.textSecond}>Open App.js to start working on your app a SECOND time!</Text>
-        </View>
-
-        <View style={styles.innerBoxThird}>
-          <Text style={styles.textThird}>Open App.js to start working on your app a THIRD time!</Text>
-        </View>
-
-        <StatusBar style="auto" />
+        {/* <View style={styles.innerBoxFirst}> */}
+        <TextInput style={styles.input} textAlign={"center"} />
+        {/* </View> */}
 
       </ImageBackground>
     </View>
@@ -44,8 +32,7 @@ const styles = StyleSheet.create({
   //! Основной контейнер
   container: {
     flex: 1,
-    // backgroundColor: '#fff',
-    backgroundColor: '#ffd700',
+    backgroundColor: '#fff',
     // alignItems: 'center',
     // justifyContent: 'center',
   },
@@ -53,45 +40,15 @@ const styles = StyleSheet.create({
   image: {
     flex: 1,
     resizeMode: "cover",
-    alignItems: 'center',
+    // alignItems: 'center',
     justifyContent: "center"
   },
-  textFirst: {
-    color: "red",
-    fontSize: 30
-  },
-  textSecond: {
-    // color: "green",
-    color: "#00ff7f",
-    fontSize: 25
-  },
-  textThird: {
-    // color: "blue",
-    color: "#ffff00",
-    fontSize: 20
-  },
-  innerBoxFirst: {
+  input: {
     borderWidth: 1,
-    borderColor: "red",
-    borderRadius: 30,
-    padding: 40,
-    width: 300,
-    backgroundColor: "#6b8e23",
-  },
-  innerBoxSecond: {
-    borderWidth: 1,
-    borderColor: "red",
-    borderRadius: 30,
-    padding: 30,
-    width: 325,
-    backgroundColor: "#db7093",
-  },
-  innerBoxThird: {
-    borderWidth: 1,
-    borderColor: "red",
-    borderRadius: 30,
-    padding: 20,
-    width: 350,
-    backgroundColor: "#61dafb",
+    borderColor: "#f0f8ff",
+    height: 40,
+    borderRadius: 10,
+    marginHorizontal: 40,
+    color: "#f0f8ff",
   },
 });
