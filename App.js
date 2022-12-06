f4f5ef3da2b97f212b3a02e420309e0dc28580cc
Ruslan379 <arnov@i.ua>
2022-12-06 21:15:00 +0200

    Start HW-2 *14

--- App.js
+++ App.js
@@ -9,9 +9,12 @@ export default function App() {
       <Text style={{ color: "red", fontSize: 25 }}>Open up App.js to start working on your app twice!</Text>
       <Text style={{ color: "blue", fontSize: 30 }}>Open up App.js to start working on your app thrice!</Text> */}
 
-      <Text style={{ color: "green", fontSize: 20 }}>Open up App.js to start working on your app!</Text>
-      <Text style={{ color: "red", fontSize: 25 }}>Open up App.js to start working on your app twice!</Text>
-      <Text style={{ color: "blue", fontSize: 30 }}>Open up App.js to start working on your app thrice!</Text>
+      <View style={styles.innerBox}>
+        <Text style={styles.textFirst}>Open up App.js to start working on your app!</Text>
+      </View>
+
+      <Text style={styles.textTwice}>Open up App.js to start working on your app twice!</Text>
+      <Text style={styles.textThrice}>Open up App.js to start working on your app thrice!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -25,5 +28,23 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-
+  textFirst: {
+    color: "red",
+    fontSize: 40
+  },
+  textTwice: {
+    color: "green",
+    fontSize: 30
+  },
+  textThrice: {
+    color: "blue",
+    fontSize: 20
+  },
+  innerBox: {
+    borderWidth: 1,
+    borderColor: "red",
+    borderRadius: 10,
+    padding: 40,
+    width: 300
+  },
 });
