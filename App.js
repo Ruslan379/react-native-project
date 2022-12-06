79ff50e966a45f9f2312efd65716cecfae6a25f9
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2022-12-06 15:01:41 +0200

    Start HW-2 *11

--- App.js
+++ App.js
@@ -4,9 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>Open up App.js to start working on your app twice!</Text>
-      <Text>Open up App.js to start working on your app thrice!</Text>
+      <Text style={{ color: "green" }}>Open up App.js to start working on your app!</Text>
+      <Text style={{ color: "red" }}>Open up App.js to start working on your app twice!</Text>
+      <Text style={{ color: "blue" }}>Open up App.js to start working on your app thrice!</Text>
       <StatusBar style="auto" />
     </View>
   );
