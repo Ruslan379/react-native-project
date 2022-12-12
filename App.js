ee1d42efc97b21d9d0d8a3e1481c509a2d91aa42
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2022-12-12 15:03:24 +0200

    Work HW-4 *85

--- App.js
+++ App.js
@@ -34,8 +34,8 @@ export default function App() {
   return (
     <NavigationContainer>
       {/* <AuthStack.Navigator> */}
-      {/* <AuthStack.Screen name="Register" component={RegisterScreen} /> */}
-      {/* <AuthStack.Screen name="Login" component={LoginScreen} /> */}
+      <AuthStack.Screen name="Register" component={RegistrationScreen} />
+      <AuthStack.Screen name="Login" component={LoginScreen} />
       {/* </AuthStack.Navigator> */}
 
       <RegistrationScreen />
