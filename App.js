6db1fbd7c860fadf76381524c8b1f24c98f60eb2
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2022-12-12 20:59:11 +0200

    Work HW-4_4.9 *92

--- App.js
+++ App.js
@@ -3,55 +3,12 @@ import React, { useState } from "react";
 import { useFonts } from "expo-font";
 
 import { NavigationContainer } from '@react-navigation/native';
-// import { createStackNavigator } from '@react-navigation/stack';
-// import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 import { useRoute } from "./router";
 
-// //!  auth
-// import RegistrationScreen from "./Screens/auth/RegistrationScreen.js";
-// import LoginScreen from "./Screens/auth/LoginScreen.js";
-// //!  mainScreen
-// import PostsScreen from "./Screens/mainScreen/PostsScreen";
-// import CreateScreen from "./Screens/mainScreen/CreateScreen";
-// import ProfileScreen from "./Screens/mainScreen/ProfileScreen";
-
 
 // --------------------------------------------------------------------------------------
-// const AuthStack = createStackNavigator();
-// const MainTab = createBottomTabNavigator();
 
-// //! Логика отрисовки страниц auth или mainScreen
-// const useRoute = (isAuth) => {
-//   if (!isAuth) {
-//     return (
-//       <AuthStack.Navigator>
-//         {/* //! RegistrationScreen */}
-//         <AuthStack.Screen
-//           options={{
-//             headerShown: false,
-//           }}
-//           name="Register"
-//           component={RegistrationScreen}
-//         />
-//         {/* //! LoginScreen */}
-//         <AuthStack.Screen options={{
-//           headerShown: false,
-//         }}
-//           name="Login"
-//           component={LoginScreen}
-//         />
-//       </AuthStack.Navigator>
-//     );
-//   }
-//   return (
-//     <MainTab.Navigator>
-//       <MainTab.Screen name="Posts" component={PostsScreen} />
-//       <MainTab.Screen name="Create" component={CreateScreen} />
-//       <MainTab.Screen name="Profile" component={ProfileScreen} />
-//     </MainTab.Navigator>
-//   );
-// };
 
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 export default function App() {
@@ -76,63 +33,5 @@ export default function App() {
 
   return <NavigationContainer>{routing}</NavigationContainer>;
 }
-
-
-
-  // return (
-
-  //   <NavigationContainer>
-
-  //     <AuthStack.Navigator>
-  //       //! RegistrationScreen
-  //       <AuthStack.Screen
-  //         options={{
-  //           headerShown: false,
-  //         }}
-  //         name="Register"
-  //         component={RegistrationScreen}
-  //       />
-  //       //! LoginScreen
-  //       <AuthStack.Screen options={{
-  //         headerShown: false,
-  //       }}
-  //         name="Login"
-  //         component={LoginScreen}
-  //       />
-  //     </AuthStack.Navigator>
-
-
-  //     <MainTab.Navigator>
-  //       <MainTab.Screen name="Posts" component={PostsScreen} />
-  //       <MainTab.Screen name="Create" component={CreateScreen} />
-  //       <MainTab.Screen name="Profile" component={ProfileScreen} />
-  //     </MainTab.Navigator>
-
-  //   </NavigationContainer>
-  // )
-
-
-
-
-
-// auth
-
-// <AuthStack.Navigator>
-// <AuthStack.Screen
-//   options={{
-//     headerShown: false,
-//   }}
-//   name="Login"
-//   component={LoginScreen}
-// />
-// <AuthStack.Screen
-//   options={{
-//     headerShown: false,
-//   }}
-//   name="Register"
-//   component={RegisterScreen}
-// />
-// </AuthStack.Navigator>
-
 
 
