791b28d49bee633857981713c1e952af385726d1
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2022-12-14 20:32:06 +0200

    Work HW-5_5.2 *119

--- Screens/mainScreen/CreatePostsScreen.js
+++ Screens/mainScreen/CreatePostsScreen.js
@@ -4,12 +4,12 @@ import {
   View,
   Text,
   StyleSheet,
-  // TouchableOpacity,
+  TouchableOpacity,
   Image,
 } from "react-native";
 
 import { Camera } from "expo-camera";
-import { TouchableOpacity } from "react-native-gesture-handler";
+// import { TouchableOpacity } from "react-native-gesture-handler";
 
 
 // -------------------------------------------------------------------
@@ -92,49 +92,3 @@ const styles = StyleSheet.create({
 });
 
 export default CreatePostsScreen;
-
-
-
-
-//! Конспект
-// import React from "react";
-// import { View, Text, StyleSheet } from "react-native";
-// import { Camera } from "expo-camera";
-// import { TouchableOpacity } from "react-native-gesture-handler";
-// const CreatePostsScreen = () => {
-//   return (
-//     <View style={styles.container}>
-//       <Camera style={styles.camera}>
-//         <TouchableOpacity onPress={() => { }} style={styles.snapContainer}>
-//           <Text style={styles.snap}>SNAP</Text>
-//         </TouchableOpacity>
-//       </Camera>
-//     </View>
-//   );
-// };
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//   },
-//   camera: {
-//     height: 300,
-//     marginTop: 50,
-//     alignItems: "center",
-//   },
-//   snap: {
-//     color: "#fff",
-//   },
-//   snapContainer: {
-//     marginTop: 200,
-//     borderWidth: 1,
-//     borderColor: "#ff0000",
-//     width: 70,
-//     height: 70,
-//     borderRadius: 50,
-//     justifyContent: "center",
-//     alignItems: "center",
-//   },
-// });
-
-// export default CreatePostsScreen;
