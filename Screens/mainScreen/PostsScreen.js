8f5edbb5106e7a18160356c7732aefa8d98862a3
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2022-12-14 11:59:48 +0200

    Start HW-5 *105

--- Screens/mainScreen/PostsScreen.js
+++ Screens/mainScreen/PostsScreen.js
@@ -2,6 +2,13 @@ import React from "react";
 // import { moduleName } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image //? 2.14
+} from 'react-native';
+
 //!  nestedScreens
 import Home from "../nestedScreens/Home.js";
 import CommentsScreen from "../nestedScreens/CommentsScreen.js";
@@ -11,29 +18,47 @@ const NestedScreen = createStackNavigator();
 
 const PostsScreen = () => {
   return (
-    <NestedScreen.Navigator>
-      <NestedScreen.Screen
-        // options={{
-        //   headerShown: false,
-        // }}
-        name="Home"
-        component={Home}
+    <>
+      <Image
+        source={require("../../assets/icons/log-out.png")}
+        // source={image}
+        style={{
+          // backgroundColor: "#000000",
+          color: "#ffffff",
+          width: 24,
+          height: 24,
+          // width: windowWidth,
+          // height: windowHeight,
+          // position: 'absolute',
+          // right: 0,
+          // top: 0,
+          // left: 0
+        }}
       />
-      <NestedScreen.Screen
-        // options={{
-        //   headerShown: false,
-        // }}
-        name="CommentsScreen"
-        component={CommentsScreen}
-      />
-      <NestedScreen.Screen
-        // options={{
-        //   headerShown: false,
-        // }}
-        name="MapScreen"
-        component={MapScreen}
-      />
-    </NestedScreen.Navigator>
+      <NestedScreen.Navigator>
+        <NestedScreen.Screen
+          // options={{
+          //   headerShown: false,
+          // }}
+          name="Home"
+          component={Home}
+        />
+        <NestedScreen.Screen
+          // options={{
+          //   headerShown: false,
+          // }}
+          name="CommentsScreen"
+          component={CommentsScreen}
+        />
+        <NestedScreen.Screen
+          // options={{
+          //   headerShown: false,
+          // }}
+          name="MapScreen"
+          component={MapScreen}
+        />
+      </NestedScreen.Navigator>
+    </>
   );
 };
 
