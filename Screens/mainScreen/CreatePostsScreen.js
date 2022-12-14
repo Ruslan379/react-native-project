dc9f94f31ba2d0a520434a5af864beb29be78d95
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2022-12-14 18:07:51 +0200

    Start HW-5_5.1 *113

--- Screens/mainScreen/CreatePostsScreen.js
+++ Screens/mainScreen/CreatePostsScreen.js
@@ -1,10 +1,22 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { Camera } from "expo-camera";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 const CreatePostsScreen = () => {
   return (
     <View style={styles.container}>
-      <Text>Create Posts Screen</Text>
+      {/* <Text>Create Posts Screen</Text> */}
+      <Camera style={styles.camera}>
+        <TouchableOpacity
+          style={styles.snapContainer}
+          onPress={() => { }}
+        >
+          <Text style={styles.snap}>SNAP</Text>
+        </TouchableOpacity>
+      </Camera>
+
+
     </View>
   );
 };
@@ -12,6 +24,24 @@ const CreatePostsScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    // justifyContent: "center",
+    // alignItems: "center",
+  },
+  camera: {
+    height: 300,
+    marginTop: 50,
+    alignItems: "center",
+  },
+  snap: {
+    color: "#fff",
+  },
+  snapContainer: {
+    marginTop: 200,
+    borderWidth: 1,
+    borderColor: "#ff0000",
+    width: 70,
+    height: 70,
+    borderRadius: 50,
     justifyContent: "center",
     alignItems: "center",
   },
