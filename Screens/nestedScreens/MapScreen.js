78e1cc2ca4deeb3efcbe1fa30ad19f802099411a
Ruslan379 <arnov@i.ua>
2022-12-15 16:13:54 +0200

    Final HW-5 *128

--- Screens/nestedScreens/MapScreen.js
+++ Screens/nestedScreens/MapScreen.js
@@ -1,10 +1,25 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import MapView, { Marker } from "react-native-maps";
 
 const MapScreen = () => {
   return (
     <View style={styles.container}>
-      <Text>Map Screen</Text>
+      {/* <Text>Map Screen</Text> */}
+      <MapView
+        style={{ flex: 1 }}
+        initialRegion={{
+          latitude: 50.5262122,
+          longitude: 30.5991799,
+          latitudeDelta: 0.001,
+          longitudeDelta: 0.006,
+        }}
+      >
+        <Marker
+          coordinate={{ latitude: 50.5262122, longitude: 30.5991799 }}
+          title="travel photo"
+        />
+      </MapView>
     </View>
   );
 };
@@ -13,8 +28,8 @@ const MapScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    // justifyContent: "center",
+    // alignItems: "center",
   },
 });
 
