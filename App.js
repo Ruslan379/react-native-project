5972d20d05c14eb11bae6542e0bc403e64d72d3a
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2022-12-17 01:57:37 +0200

    Work HW-6_6.9 *156

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from "react";
+// import React, { useState, useEffect, useCallback } from "react";
 import { Provider, useSelector } from "react-redux";
 import { useFonts } from "expo-font";
 
