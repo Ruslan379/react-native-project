cdfda75da211e05fbf567466bc41572ebeaa73d0
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2022-12-16 22:01:35 +0200

    Work HW-6_6.5 *147

--- App.js
+++ App.js
@@ -9,12 +9,28 @@ import { NavigationContainer } from '@react-navigation/native';
 import { store } from "./redux/store";
 import { useRoute } from "./router";
 
+//! ---- Логика отрисовки страниц auth или mainScreen c Firebase ----
+// import db from "./firebase/config"; //todo --> устарело!!!
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import app from "./firebase/config.js";
+
+const auth = getAuth(app);
+//! ____ Логика отрисовки страниц auth или mainScreen c Firebase ____
 
 // --------------------------------------------------------------------------------------
 
 
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 export default function App() {
+  //! ---- Логика отрисовки страниц auth или mainScreen c Firebase ----
+  const [user, setUser] = useState(null);
+
+  // db.auth().onAuthStateChanged((user) => setUser(user)); //todo --> устарело!!!
+  onAuthStateChanged(auth, (user) => setUser(user));
+  console.log("App==>user:", user); //!
+  //! ____ Логика отрисовки страниц auth или mainScreen c Firebase ____
+
+
   //! Загрузка шрифтов_NEW
   const [fontsLoaded] = useFonts({
     "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
@@ -22,12 +38,18 @@ export default function App() {
     "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
   });
 
+
   console.log("App ==>", Platform.OS); //!
 
+
   //! Логика отрисовки страниц auth или mainScreen 
-  const routing = useRoute(false); //! auth
+  // const routing = useRoute(false); //! auth
   // const routing = useRoute(true); //! mainScreen
-  // 
+  //! ---- Логика отрисовки страниц auth или mainScreen c Firebase ----
+  const routing = useRoute(user); //! mainScreen
+  //! ____ Логика отрисовки страниц auth или mainScreen c Firebase ____
+
+
   //! Проверка наличия шрифтов
   // console.log("fontsLoaded:", fontsLoaded); //!
   if (!fontsLoaded) {
--- redux/auth/users-registered-list.js
+++ redux/auth/users-registered-list.js
@@ -2,13 +2,13 @@ User1 = {
     nickname: "Testuser",
     email: "testuser@ukr.net",
     password: "Q1w2e3",
-    UID: "JULRJMkUuebZPDQl2HoJ3LhCwfG2",
+    UID: "bF4eELO1sTMOqJcRBpUN3dx5izi2",
 }
 User2 = {
     nickname: "Rony Dagger",
     email: "Rony_daggerr@ukr.net",
     password: "Z1x2c3",
-    UID: "bF4eELO1sTMOqJcRBpUN3dx5izi2",
+    UID: "JULRJMkUuebZPDQl2HoJ3LhCwfG2",
 }
 User3 = {
     nickname: "Dolli Wizard",
