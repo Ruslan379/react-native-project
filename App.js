6e4747833c356640a8a52dc88c8a54c29a7aa845
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2022-12-17 01:32:37 +0200

    Work HW-6_6.8 *153

--- App.js
+++ App.js
@@ -1,18 +1,18 @@
 import React, { useState, useEffect, useCallback } from "react";
 import { Provider, useSelector } from "react-redux";
 import { useFonts } from "expo-font";
-// import { NavigationContainer } from '@react-navigation/native';
+// import { NavigationContainer } from '@react-navigation/native'; //?
 
 import { store } from "./redux/store";
-// import { useRoute } from "./routes/router";
+// import { useRoute } from "./routes/router"; //?
 
-// //! ---- Логика входа в "auth" или в "mainScreen" c Firebase ----
+// //? ---- Логика входа в "auth" или в "mainScreen" c Firebase ----
 // // import db from "./firebase/config"; //todo --> устарело!!!
 // import { getAuth, onAuthStateChanged } from "firebase/auth";
 // import app from "./firebase/config.js";
 
 // const auth = getAuth(app);
-// //! ____ Логика входа в "auth" или в "mainScreen" c Firebase ____
+// //? ____ Логика входа в "auth" или в "mainScreen" c Firebase ____
 
 import { Main } from "./components/Main.js";
 
@@ -21,13 +21,14 @@ import { Main } from "./components/Main.js";
 
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 export default function App() {
-  // //! ---- Логика входа в "auth" или в "mainScreen" c Firebase ----
+
+  // //? ---- Логика входа в "auth" или в "mainScreen" c Firebase ----
   // const [user, setUser] = useState(null);
 
   // // db.auth().onAuthStateChanged((user) => setUser(user)); //todo --> устарело!!!
   // onAuthStateChanged(auth, (user) => setUser(user));
   // console.log("App==>user:", user); //!
-  // //! ____ Логика входа в "auth" или в "mainScreen" c Firebase ____
+  // //? ____ Логика входа в "auth" или в "mainScreen" c Firebase ____
 
 
   //! Загрузка шрифтов_NEW
@@ -41,12 +42,12 @@ export default function App() {
   console.log("App ==>", Platform.OS); //!
 
 
-  //! Логика входа в "auth" или в "mainScreen" 
+  //? Логика входа в "auth" или в "mainScreen" 
   // const routing = useRoute(false); //? auth
   // const routing = useRoute(true); //* mainScreen
   //! ---- Логика входа в "auth" или в "mainScreen" c Firebase ----
   // const routing = useRoute(user);
-  //! ____ Логика входа в "auth" или в "mainScreen" c Firebase ____
+  //? ____ Логика входа в "auth" или в "mainScreen" c Firebase ____
 
 
   //! Проверка наличия шрифтов
@@ -58,7 +59,7 @@ export default function App() {
   return (
     <Provider store={store}>
       <Main />
-      {/* <NavigationContainer>{routing}</NavigationContainer> */}
+      {/* //? <NavigationContainer>{routing}</NavigationContainer> */}
     </Provider>
   );
 }
