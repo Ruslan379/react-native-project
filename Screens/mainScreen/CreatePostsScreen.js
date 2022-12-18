d6f86dca56b44b23acb1e3672f4755235ad4f3ef
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2022-12-18 18:41:21 +0200

    Work HW-6_6.15 *169

--- Screens/mainScreen/CreatePostsScreen.js
+++ Screens/mainScreen/CreatePostsScreen.js
@@ -59,7 +59,8 @@ const CreatePostsScreen = ({ navigation }) => {
         setErrorMsg("Permission to access location was denied");
         return;
       }
-      let location = await Location.getCurrentPositionAsync({}); //?
+      // let location = await Location.getCurrentPositionAsync({}); //?
+      const location = await Location.getCurrentPositionAsync(); //?
       setLocation(location); //?
     })();
   }, []);
