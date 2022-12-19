b7bc97a255caae2531dcad58a8ad30f4752a769c
Ruslan <103874267+Ruslan379@users.noreply.github.com>
2022-12-19 02:14:11 +0200

    Work HW-6_6.19 *192

--- Screens/mainScreen/ProfileScreen.js
+++ Screens/mainScreen/ProfileScreen.js
@@ -46,7 +46,7 @@ const ProfileScreen = () => {
       collectionByUserId,
       (data) => {
         console.log(data.docs.map((doc) => ({ ...doc.data(), id: doc.id }))); //!
-        // setUserPosts(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+        setUserPosts(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
       });
   };
 
@@ -123,15 +123,7 @@ const ProfileScreen = () => {
 
 //! JS-объект со стилями
 const styles = StyleSheet.create({
-  //! Кнопка Log-out
-  btnLogOut: {
-    marginTop: 54,
-    marginRight: 16,
-    marginBottom: 10,
-    justifyContent: "center",
-    alignItems: "flex-end",
-    // backgroundColor: "#FFFFFF",
-  },
+
   //! Основной контейнер
   container: {
     flex: 1,
@@ -166,56 +158,11 @@ const styles = StyleSheet.create({
     marginTop: -60,
     borderRadius: 16,
   },
-  //! Контейнер: form
-  form: {
-    alignItems: 'center',
-  },
-  //! текст: Регистрация
-  headerTitle: {
-    color: "#000000",
-    fontSize: 30,
-    marginTop: 32,
-    fontFamily: "Roboto-Medium",
-  },
-  //! Input: Логин + Email + Пароль
-  input: {
-    marginHorizontal: 16,
-    paddingLeft: 16,
-    width: 343,
-    // width: '100%',
-    height: 50,
-    borderColor: "#E8E8E8",
-    borderWidth: 1,
-    borderRadius: 8,
-    placeholderTextColor: "#BDBDBD",
-    backgroundColor: "#F6F6F6",
-    fontFamily: "Roboto-Regular",
-  },
-  //! Кнопка: Зарегистрироваться
-  btnSubmit: {
-    marginTop: 43,
-    height: 51,
-    width: 343,
-    backgroundColor: "#FF6C00",
-    justifyContent: "center",
-    alignItems: "center",
-    borderRadius: 100,
-  },
-  //! Текст кнопки: Зарегистрироваться
-  btnSubmitTitle: {
-    fontSize: 16,
-    color: "#FFFFFF",
-    fontFamily: "Roboto-Regular",
-  },
-  //! Кнопка: Уже есть аккаунт? Войти
-  goToLoginPage: {
-    alignItems: "center",
-    marginTop: 16,
-  },
-  //! Текст кнопки: Уже есть аккаунт? Войти
-  goToLoginPageText: {
-    color: "#000000",
-    fontFamily: "Roboto-Regular",
+  //! Кнопка Log-out
+  btnLogOut: {
+    top: 22,
+    right: 16,
+    position: "absolute",
   },
 });
 
